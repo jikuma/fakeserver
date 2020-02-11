456db1b6bd0c7975aea4e12f9e92f7d1d1966ff0
jikuma <jikuma@microsoft.com>
2020-02-11 14:12:42 -0800

    first checkin

---
+++ MyFakeServer/Controllers/MyFakeServerController.cs
@@ -0,0 +1,210 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace MyFakeServer.Controllers
+{
+    [ApiController]
+    [Route("server")]
+    public class MyFakeServerController
+    {
+        [HttpGet]
+        [Route("{serverID}/{*url}")]
+        public async Task<ContentResult> Get(string serverID, string url)
+        {
+            bool isValid = Guid.TryParse(serverID, out Guid guidOutput);
+            if(!isValid)
+            {
+                return new ContentResult()
+                {
+                    StatusCode = (int)HttpStatusCode.BadRequest,
+                };
+            }
+
+            if(!RegisterController.data.ContainsKey(serverID))
+            {
+                return new ContentResult()
+                {
+                    StatusCode = 404,
+                    Content = "server id not registered.",
+                };
+            }
+
+            if (!RegisterController.data[serverID].ContainsKey(url))
+            {
+                return new ContentResult()
+                {
+                    StatusCode = 404,
+                    Content = "url not registered.",
+                };
+            }
+
+            if (!RegisterController.data[serverID][url].ContainsKey("GET"))
+            {
+                return new ContentResult()
+                {
+                    StatusCode = 404,
+                    Content = "METHOD not registered.",
+                };
+            }
+
+
+            return new ContentResult()
+            {
+                StatusCode = 200,
+                Content = RegisterController.data[serverID][url]["GET"],
+                ContentType = "application/json"
+            };
+        }
+
+        [HttpPost]
+        [Route("{serverID}/{*url}")]
+        public async Task<ContentResult> Post(string serverID, string url)
+        {
+            bool isValid = Guid.TryParse(serverID, out Guid guidOutput);
+            if (!isValid)
+            {
+                return new ContentResult()
+                {
+                    StatusCode = (int)HttpStatusCode.BadRequest,
+                };
+            }
+
+            if (!RegisterController.data.ContainsKey(serverID))
+            {
+                return new ContentResult()
+                {
+                    StatusCode = 404,
+                    Content = "server id not registered.",
+                };
+            }
+
+            if (!RegisterController.data[serverID].ContainsKey(url))
+            {
+                return new ContentResult()
+                {
+                    StatusCode = 404,
+                    Content = "url not registered.",
+                };
+            }
+
+            if (!RegisterController.data[serverID][url].ContainsKey("Post"))
+            {
+                return new ContentResult()
+                {
+                    StatusCode = 404,
+                    Content = "METHOD not registered.",
+                };
+            }
+
+
+            return new ContentResult()
+            {
+                StatusCode = 200,
+                Content = RegisterController.data[serverID][url]["Post"],
+                ContentType = "application/json"
+            };
+        }
+
+        [HttpPatch]
+        [Route("{serverID}/{*url}")]
+        public async Task<ContentResult> Patch(string serverID, string url)
+        {
+            bool isValid = Guid.TryParse(serverID, out Guid guidOutput);
+            if (!isValid)
+            {
+                return new ContentResult()
+                {
+                    StatusCode = (int)HttpStatusCode.BadRequest,
+                };
+            }
+
+            if (!RegisterController.data.ContainsKey(serverID))
+            {
+                return new ContentResult()
+                {
+                    StatusCode = 404,
+                    Content = "server id not registered.",
+                };
+            }
+
+            if (!RegisterController.data[serverID].ContainsKey(url))
+            {
+                return new ContentResult()
+                {
+                    StatusCode = 404,
+                    Content = "url not registered.",
+                };
+            }
+
+            if (!RegisterController.data[serverID][url].ContainsKey("Patch"))
+            {
+                return new ContentResult()
+                {
+                    StatusCode = 404,
+                    Content = "METHOD not registered.",
+                };
+            }
+
+
+            return new ContentResult()
+            {
+                StatusCode = 200,
+                Content = RegisterController.data[serverID][url]["Patch"],
+                ContentType = "application/json"
+            };
+        }
+
+        [HttpPut]
+        [Route("{serverID}/{*url}")]
+        public async Task<ContentResult> Put(string serverID, string url)
+        {
+            bool isValid = Guid.TryParse(serverID, out Guid guidOutput);
+            if (!isValid)
+            {
+                return new ContentResult()
+                {
+                    StatusCode = (int)HttpStatusCode.BadRequest,
+                };
+            }
+
+            if (!RegisterController.data.ContainsKey(serverID))
+            {
+                return new ContentResult()
+                {
+                    StatusCode = 404,
+                    Content = "server id not registered.",
+                };
+            }
+
+            if (!RegisterController.data[serverID].ContainsKey(url))
+            {
+                return new ContentResult()
+                {
+                    StatusCode = 404,
+                    Content = "url not registered.",
+                };
+            }
+
+            if (!RegisterController.data[serverID][url].ContainsKey("Put"))
+            {
+                return new ContentResult()
+                {
+                    StatusCode = 404,
+                    Content = "METHOD not registered.",
+                };
+            }
+
+
+            return new ContentResult()
+            {
+                StatusCode = 200,
+                Content = RegisterController.data[serverID][url]["Put"],
+                ContentType = "application/json"
+            };
+        }
+    }
+}
---
+++ MyFakeServer/Controllers/RegisterController.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace MyFakeServer.Controllers
+{
+    [ApiController]
+    [Route("register")]
+    public class RegisterController
+    {
+        public static IDictionary<string, IDictionary<string, IDictionary<string, string>>> data = new Dictionary<string, IDictionary<string, IDictionary<string, string>>>(StringComparer.OrdinalIgnoreCase);
+
+        [HttpPost]
+        [Route("{serverID}/{*url}")]
+        public async Task<ContentResult> Post(string serverID, string url, [FromBody] dynamic input, [FromQuery] string method)
+        {
+            bool isValid = Guid.TryParse(serverID, out Guid guidOutput);
+            if (!isValid)
+            {
+                return new ContentResult()
+                {
+                    StatusCode = (int)HttpStatusCode.BadRequest,
+                    Content = "server id should be guid"
+                };
+            }
+
+            if(string.IsNullOrEmpty(method))
+            {
+                method = "GET";
+            }
+
+            if(!(method.Equals("GET", StringComparison.OrdinalIgnoreCase) || method.Equals("PUT", StringComparison.OrdinalIgnoreCase) || method.Equals("PATCH", StringComparison.OrdinalIgnoreCase)) || method.Equals("POST", StringComparison.OrdinalIgnoreCase))
+            {
+                return new ContentResult()
+                {
+                    StatusCode = (int)HttpStatusCode.BadRequest,
+                    Content = "supported method is only get, put, post, patch"
+                };
+            }
+
+            if(!data.ContainsKey(serverID))
+            {
+                data[serverID] = new Dictionary<string, IDictionary<string, string>>(StringComparer.OrdinalIgnoreCase);
+            }
+
+            if(!data[serverID].ContainsKey(url))
+            {
+                data[serverID][url] = new Dictionary<string, string>(StringComparer.OrdinalIgnoreCase);
+            }
+
+            data[serverID][url][method] = Convert.ToString(input);
+            return new ContentResult()
+            {
+                StatusCode = 200,
+                ContentType = "application/json",
+                Content = data[serverID][url][method]
+            };
+        }
+    }
+}
